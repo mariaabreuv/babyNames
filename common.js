1581fd4c57fef994948d5f295591cf2116bc0dcc
mariabreuv <17maria.abreu@gmail.com>
2024-11-20 00:37:44 +0000

    added top 10 baby names graph on top10babies branch

--- common.js
+++ common.js
@@ -1,48 +1,97 @@
-let svg
-let canvasHeight, canvasWidth, padding, graphWidth, graphHeight, radius, max_pop, max_nMovies, max_nAwards;
-let babies
+let svg;
+let canvasHeight, canvasWidth, padding, graphWidth, graphHeight;
+let babies;
 
 window.onload = function () {
-    //general graph attr
+    // General graph attributes
     canvasHeight = 980;
     canvasWidth = 1080;
     padding = 60;
     graphHeight = canvasHeight - padding * 2;
     graphWidth = canvasWidth - padding * 2;
 
-    babies = './top10babies.csv'
-    //create a tootil, for data information
+    babies = './top10babies.csv'; // Path to your dataset
 
-    //new svg element
+    // Create an SVG element
     svg = d3.select('body')
         .append('svg')
         .attr('width', canvasWidth)
         .attr('height', canvasHeight);
 
-    d3.csv(babies, b => {
-        return {
-            year: +b.Year_of_Birth, //make this  a number
-            gender: b.Gender,
-            name: b.Name,
-            rank: b.Rank,
-            count: b.Baby_Count,
-            total: b.Total,
-            Percentage: b.Percentage,
-        }
-    }).then(smallMultiples);
-}
+    // Load and parse the CSV file
+    d3.csv(babies, b => ({
+        year: +b.Year_of_Birth, // Convert year to a number
+        gender: b.Gender,
+        name: b.Name,
+        rank: +b.Rank,
+        count: +b.Baby_Count, // Baby count
+        total: +b.Total,
+        percentage: +b.Percentage
+    })).then(top10);
+};
 
-function smallMultiples(data) {
-    //grouped by Year and subgroup gender
-    let yearGender = d3.groups(data, d => d.year, d => d.gender)
-    
-    //grouped by first letter
-    let yearName = d3.groups(data, d => d.name[0])
+function top10(data) {
+    // Filter top 10 names based on baby count
+    let top10Names = data
+        .filter(d => d.rank <= 10)
+        .sort((a, b) => d3.ascending(a.year, b.year));
 
-    // Print the grouped data to check
-    //console.log(yearGender,yearName);
+    // Group data by name
+    let dataByName = d3.groups(top10Names, d => d.name);
 
+    // Create scales
+    let xScale = d3.scaleLinear()
+        .domain(d3.extent(data, d => d.year))
+        .range([padding, graphWidth]);
 
+    let yScale = d3.scaleLinear()
+        .domain([0, d3.max(data, d => d.count)])
+        .range([graphHeight, padding]);
 
+    // Create axes
+    let xAxis = d3.axisBottom(xScale).ticks(10).tickFormat(d3.format("d"));
+    let yAxis = d3.axisLeft(yScale).ticks(10);
 
+    // Append axes to the SVG
+    svg.append("g")
+        .attr("transform", `translate(0, ${graphHeight})`)
+        .call(xAxis);
+
+    svg.append("g")
+        .attr("transform", `translate(${padding}, 0)`)
+        .call(yAxis);
+
+    // Define a line generator
+    let line = d3.line()
+        .x(d => xScale(d.year))
+        .y(d => yScale(d.count));
+
+    // Draw lines for each name
+    dataByName.forEach(([name, values]) => {
+        svg.append("path")
+            .datum(values)
+            .attr("fill", "none")
+            .attr("stroke", d3.schemeCategory10[Math.floor(Math.random() * 10)]) // Random colors
+            .attr("stroke-width", 2)
+            .attr("d", line);
+    });
+
+    // Add a subtitles
+    let legend = svg.selectAll(".legend")
+        .data(dataByName)
+        .enter()
+        .append("g")
+        .attr("transform", (d, i) => `translate(${graphWidth + 50}, ${padding + i * 20})`);
+
+    legend.append("rect")
+        .attr("width", 10)
+        .attr("height", 10)
+        .attr("fill", (_, i) => d3.schemeCategory10[i % 10]);
+
+    legend.append("text")
+        .attr("x", 15)
+        .attr("y", 10)
+        .text(d => d[0])
+        .attr("font-size", "12px")
+        .attr("fill", "black");
 }
